3e65ddefa0221ab28d97a5ca9d33ada4343d77e6
Sudheendra <sudheendra579@gmail.com>
2026-05-26 00:33:32 +0530

    added deployed api url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: process.env.REACT_APP_API_URL,
     headers: {
         'Content-Type': 'application/json',
     }
